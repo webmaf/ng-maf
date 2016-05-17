f9cc93754e95384801332b2974f6454677daef98
Matthias Fischer <matthias.fischer@tui.com>
2016-05-17 15:30:04 +0200

    clean project

--- config/css-tags.js
+++
@@ -1,7 +0,0 @@
-// @if ENV='DEV'
-<link rel="stylesheet" type="text/css" href="http://local.tui.de:9001/assets/css/screen-ext.css/* @echo MS */"/>
-// @endif
-
-// @if ENV='PROD'
-<link rel="stylesheet" type="text/css" href="/assets/css/screen-ext.css"/>
-// @endif
