b8bb623890b61141523155d9b2cb6cc60f173167
Matthias Fischer <matthias.fischer@tui.com>
2016-08-05 15:33:58 +0200

    disable steam.module

--- app/src/steam.module.js
+++ app/src/steam.module.js
@@ -5,12 +5,12 @@
         .module('steam', []);
 
 
-    angular
-        .module('steam')
-        .controller('steamCtrl', function () {
-            console.log(3, 'steamCtrl');
-        })
-        .config(function () {
-            console.log(3, 'steamConfig');
-        });
+    //angular
+    //    .module('steam')
+    //    .controller('steamCtrl', function () {
+    //        console.log(3, 'steamCtrl');
+    //    })
+    //    .config(function () {
+    //        console.log(3, 'steamConfig');
+    //    });
 }());
